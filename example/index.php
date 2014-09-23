6d65b91db677bb0940ef075143910840b5e9e012
Daniel Fernando Lourusso <dflourusso@gmail.com>
2014-09-23 14:56:09 -0300

    add example folder

---
+++ example/index.php
@@ -0,0 +1,17 @@
+<div id="map-canvas" style="float:left;width:70%;height:100%;"></div>
+<div id="control_panel" style="float:right;width:30%;text-align:left;padding-top:20px; padding-left: 20px;">
+    <button class="btn btn-primary" type="button"
+            onclick="calcRoute(
+                -23.426868,
+                -51.9408231,
+                [
+                    'Rua Itamar Orlando Soares',
+                    {latitude: -23.428239, longitude: -51.9728931},
+                    'Av. Das Grevíleas',
+                    'Av. Pintassilgo'
+                ]
+            );
+            ">
+        Desenhar Rota
+    </button>
+</div>
